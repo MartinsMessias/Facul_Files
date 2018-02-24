d2ac37822448dac840ee07f3c73ebd6d8981c8a3
Messias Martins <35672212+MartinsMessias@users.noreply.github.com>
2018-02-24 12:29:52 -0500

    Update po.py
    PO ex2

--- po.py
+++ po.py
@@ -3,17 +3,17 @@ Autor:
 	https://github.com/MartinsMessias
 """
 
-material = 500
-horas = 600
-t1preco = 50
-t2preco = 40
-t3preco = 55
-t1horas = 7
-t2horas = 6
-t3horas = 8
-t1mater = 8
-t2mater = 4
-t3mater =3
+material = 500	# Quantidade de material max
+horas = 600	# Quantidade de horas max
+t1preco = 50	# Preço da porta tipo 1
+t2preco = 40	# Preço da porta tipo 2
+t3preco = 55	# Preço da porta tipo 3
+t1horas = 7	# Tempo de produção porta 1
+t2horas = 6	# Tempo de produção porta 2
+t3horas = 8	# Tempo de produção porta 3
+t1mater = 8	# material usado na porta 1
+t2mater = 4	# material usado na porta 2
+t3mater =3	# material usado na porta 3
 
 while True:
 	print("_"*20, "\n\033[01;37mPesquisa Operacional\n")
@@ -25,22 +25,22 @@ while True:
 	quantp2 = int(input("Quantidade de portas tipo 2: "))
 	quantp3 = int(input("Quantidade de portas tipo 3: "))
 
-	qm1 = quantp1 * t1mater
-	qm2 = quantp2 * t2mater
-	qm3 = quantp3 * t3mater
+	qm1 = quantp1 * t1mater	# Total de material porta 1
+	qm2 = quantp2 * t2mater	# Total de material porta 2
+	qm3 = quantp3 * t3mater	# Total de material porta 3
 
-	qh1 = quantp1 * t1horas
-	qh2 = quantp2 * t2horas
-	qh3 = quantp3 * t3horas
+	qh1 = quantp1 * t1horas	# Total de horas porta 1
+	qh2 = quantp2 * t2horas	# Total de horas porta 2
+	qh3 = quantp3 * t3horas	# Total de horas porta 3
 
-	pr1 = quantp1 * t1preco
-	pr2 = quantp2 * t2preco
-	pr3 = quantp3 * t3preco
+	pr1 = quantp1 * t1preco	# Total de lucro sobre porta 1
+	pr2 = quantp2 * t2preco	# Total de lucro sobre porta 2
+	pr3 = quantp3 * t3preco	# Total de lucro sobre porta 3
 
-	tpt = quantp1 + quantp2 + quantp3
-	thp = qh1 + qh2 + qh3
-	tmt = qm1 + qm2 + qm3
-	luc = pr1 + pr2 + pr3
+	tpt = quantp1 + quantp2 + quantp3 # Total de portas
+	thp = qh1 + qh2 + qh3	# Total de horas
+	tmt = qm1 + qm2 + qm3	# Total de material
+	luc = pr1 + pr2 + pr3	# Total de lucro
 
 	print("Foram produzidas \033[01;32m", tpt, " portas\033[00;37m")
 	print("\033[01;37mEm \033[01;32m", thp, " horas\033[00;37m")
