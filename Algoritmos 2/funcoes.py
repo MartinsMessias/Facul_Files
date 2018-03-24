1a844ef52a80f0d89f7afac165e7ffc89592ab0e
Messias Martins <35672212+MartinsMessias@users.noreply.github.com>
2018-03-24 01:39:06 -0500

    Add files via upload

--- Algoritmos 2/funcoes.py
+++ Algoritmos 2/funcoes.py
@@ -7,6 +7,7 @@ from time import sleep  # Usado para deixar um delay entre os menus
 
 produtos = []  # Lista onde serão salvos os dados de produtos
 ht = 38 * '#'
+hs = 8 * '-'
 limpatela = system('cls' if name == 'nt' else 'clear')  # Limpa a tela em GNU/Linux e Windows
 
 '''
@@ -20,27 +21,28 @@ def cadastrarProduto():
     while True:
         print(f"\n{ht}\n{'[C A D A S T R A R   P R O D U T O]':^38}\n{ht}\n")
 
-        code = int(input('[Código do produto\t]: '))
+        code = int(input(f'[CÓDIGO DO PRODUTO]{hs}[ '))
 
         for x in range(len(produtos)):
-            if code in produtos:
-                print('Já existe um produto com este código!!!\n')
-                r = str(input('Deseja atualizar as informações? [s/n]')).lower()
+            if code in produtos[x]:
+                print('\nJá existe um produto com este código!!!')
+                r = str(input('\nDeseja atualizar as informações dele? [s/n]: ')).lower()
 
-                '''
-                 FALTA TERMINAR AQUI ONDE DEVE ATUALIZAR AS INFORMAÇÕES DO PRODUTO
-                 CASO JÁ EXISTA UM E O USUÁRIO DESEJE CADASTRAR NOVAMENTE
-                '''
+                #####################################################################
+                # FALTA TERMINAR AQUI ONDE DEVE ATUALIZAR AS INFORMAÇÕES DO PRODUTO #
+                # CASO JÁ EXISTA UM E O USUÁRIO DESEJE CADASTRAR NOVAMENTE          #
+                #####################################################################
+
                 if r == 'n':
                     break
 
-        nome = str(input('[Nome do produto\t]: '))
-        valor = float(input('[Valor de venda\t\t]$ '))
-        estoqMin = int(input('[Estoque min\t\t]: '))
-        estoqAtual = int(input('[Estoque atual\t\t]: '))
+        nome = str(input(f'[NOME DO PRODUTO  ]{hs}[ '))
+        valor = float(input(f'[VALOR DE VENDA   ]{hs}[ R$ '))
+        estoqMin = int(input(f'[ESTOQUE MÍNIMO   ]{hs}[ '))
+        estoqAtual = int(input(f'[ESTOQUE ATUAL    ]{hs}[ '))
         produtos.append([code, nome, valor, estoqMin, estoqAtual])
 
-        print(f"\n{'PRODUTO CADASTRADO COM SUCESSO':^38}\n")
+        print(f"\n{'[  PRODUTO CADASTRADO COM SUCESSO  ]':^35}\n")
 
         sleep(0.5)
 
@@ -61,64 +63,52 @@ def cadastrarProduto():
 
 def realizarVenda():
 
-    while True:
-        listadecompra = list()
-        preco = 0
+    limpatela
+    print(f"\n{ht}\n{'[R E A L I Z A R   V E N D A]':^38}\n{ht}\n")
+    preco = 0
 
-        print(f"\n{ht}\n{'[R E A L I Z A R   V E N D A]':^38}\n{ht}\n")
+    while True:
 
         for x in range(len(produtos)):
 
-            if buscarProduto(int(input('\n[Código do produto]-----[  '))):
+            busca = buscarProduto(int(input(f'\n[CÓDIGO DO PRODUTO]{hs}[ ')))
+            if busca == True:
+                qtd = int(input(f"[QUANTIDADE{7 * ' '}]{hs}[ "))
+                produtos[x][3] -= qtd           # Retirada de produtos do estoque
+                preco += (produtos[x][2] * qtd)
+                print(f'\n[TOTAL PARCIAL    ]{hs}[ R$ {preco}')
 
-                qtd = int(input(f"[Quantidade]{12*'-'}[  "))
 
-                produtos[x][3] -= qtd  # Retirada de produtos do estoque
-
-                listadecompra.append(produtos[x][2] * qtd)
-                print('Lista ', listadecompra)
-
-                preco = (sum(listadecompra))
-                print('Preço ', preco)
-
+            r = str(input('\n||\bDeseja adicionar outro item? [S/n]: ')).lower()
+            if r == 's':
+                continue
             else:
-                r = str(input('\n\tDeseja adicionar outro item? [S/n]: ')).lower()
-                if r == 's':
-                    continue
-                else:
-                    limpatela
-                    break
+                vlp = float(input(f"\n[VALOR PAGO{7*' '}]{hs}[ R$ "))
+                print('[TROCO{3}]{0}[ R$ {1}\n[TOTAL{3}]{0}[ R$ {2}'
+                      .format(hs, abs(vlp - preco), preco, 12 * ' '))
 
-            vlp = float(input(f"\n[Valor pago]{12*'-'}[  R$ "))
-
-            print('[Troco     ]{0}[  R$ {1}\n[Total     ]{0}[  R$ {2}'
-                  .format(12 * '-', abs(vlp - preco), preco))
-
-        r = input('\n\tDeseja iniciar nova venda? [S/n]: ').lower()
-        if r == 's':
-            limpatela
-            continue
-
-        break
+            r = input('\nDeseja iniciar nova venda? [S/n]: ').lower()
+            if r == 's':
+                realizarVenda()
+            else:
+                return 0
 
 
-'''
-    FUNÇÃO QUE BUSCA O PRODUTO PELO CÓDIGO
-'''
 
+# FUNÇÃO QUE BUSCA O PRODUTO PELO CÓDIGO
 
 def buscarProduto(codprod):
-    e = False
 
+    e = False
     for x in range(len(produtos)):
 
         if codprod == produtos[x][0]:
             e = True  # Caso o produto esteja no cadastro e == True
 
-            print('[Produto   ]{0}[  {1}\n[Valor     ]{0}[  R$ {2}'
-                  .format(12 * '-', produtos[x][1], produtos[x][2]))
+            print('[PRODUTO{3}]{0}[ {1}\n[VALOR{4}]{0}[ R$ {2}'
+                  .format(hs, produtos[x][1], produtos[x][2], 10 * ' ', 12 * ' '))
             return e
 
-    if not e:  # Caso não receba True quer dizer que o produto não foi encontrado
-        print(f"\n{'PRODUTO NÃO ENCONTRADO':^38}\n")
-        return e
+
+    print(f"\n{'PRODUTO NÃO ENCONTRADO':^38}\n")
+    return e
