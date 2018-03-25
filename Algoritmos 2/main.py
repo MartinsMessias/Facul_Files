ad074822be52eaa29a0decb90e3910c6c724b472
Messias Martins <35672212+MartinsMessias@users.noreply.github.com>
2018-03-25 13:30:38 -0500

    Add files via upload

--- Algoritmos 2/main.py
+++ Algoritmos 2/main.py
@@ -1,24 +1,139 @@
-#   PYTHON 3.6
+"""
+    ALGORITMOS II - Trabalho 1
 
-from funcoes import *  # Módulo das funçoes do programa
-limpatela
+"""
 
-while True:
+import os  # Usado apenas para a função de limpar a tela
+from time import sleep  # Usado para deixar um delay entre os menus
 
-    print(f"{ht}\n{'[ M E N U   P R I N C I P A L ]':^38}\n{ht}\n")
-    resp = int(input('\n'
-                     '\t[1] - Cadastrar produto\n'
-                     '\t[2] - Realizar venda\n\n'
-                     '\t[0] - Sair\n\n{}\n::'
-                     .format(ht)))
+produtos = [[1, 'Leite', 1, 1, 1], [2, 'Pão', 1, 1, 1]]  # Lista onde são salvos os dados de produtos
+ht = 38 * '#'
+hs = 8 * '.'
 
-    if resp == 1:
-        cadastrarProduto()
-    elif resp == 2:
-        realizarVenda()
-    elif resp == 0:
-        break
-    else:
-        print('Opção inválida')
-        limpatela
-        continue
+
+#################################################################
+#   FUNÇÃO PARA VERIFICAR SE UM PRODUTO JÁ CONSTA NO CADASTRO   #
+#   E CASO SIM, ATUALIZA SEUS DADOS, CASO NÃO, FAZ O CADASTRO   #
+#################################################################
+
+def cadastrarProduto():
+
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')  # Limpa a tela
+        print(f"\n{ht}\n{'[C A D A S T R A R   P R O D U T O]':^38}\n{ht}\n")
+
+        code = int(input(f'[CÓDIGO DO PRODUTO]{hs}[ '))
+
+        for i in produtos:
+            if code == i[0]:
+                print('\nJá existe um produto com este código!!!')
+                r = str(input('\nDeseja atualizar as informações dele? [s/n]: ')).lower()
+                if r == 'n':
+                    menu()
+                if r == 's':
+                    produtos.remove(i)
+
+        nome = str(input(f'[NOME DO PRODUTO  ]{hs}[ '))
+        valor = float(input(f'[VALOR DE VENDA   ]{hs}[ R$ '))
+        estoqMin = int(input(f'[ESTOQUE MÍNIMO   ]{hs}[ '))
+        estoqAtual = int(input(f'[ESTOQUE ATUAL    ]{hs}[ '))
+        produtos.append([code, nome, valor, estoqMin, estoqAtual])
+
+        print(f"\n{'[  PRODUTO CADASTRADO COM SUCESSO  ]':^35}\n")
+
+        sleep(0.5)
+
+        r = str(input('\nDeseja cadastrar novo produto? [S/n]: ')).lower()
+
+        if r == 's':
+            cadastrarProduto()
+
+        return menu()
+
+
+#############################################################
+#   FUNÇÃO PARA VERIFICAR SE UM PRODUTO EXISTE NO CADASTRO  #
+#   E REALIZA A SUA VENDA CASO ENCONTRE                     #
+#############################################################
+
+def realizarVenda():
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"\n{ht}\n{'[R E A L I Z A R   V E N D A]':^38}\n{ht}\n")
+    preco = 0
+    busca = buscarProduto()
+
+    while True:
+
+        for x in range(len(produtos)):
+            if busca:
+                qtd = int(input(f"[QUANTIDADE{7 * ' '}]{hs}[ "))
+                produtos[x][3] -= qtd  # Retirada de produtos do estoque
+                preco += (produtos[x][2] * qtd)
+                print(f'\n[SUBTOTAL         ]{hs}[ R$ {preco:.2f}')
+                continue
+            if not busca:
+                break
+
+        r = str(input('\nDeseja adicionar outro item? [S/n]: ')).lower()
+        if r == 's':
+            if buscarProduto():
+                continue
+        else:
+            continue
+
+        vlp = float(input(f"\n[VALOR PAGO{7*' '}]{hs}[ R$ "))
+        print('[TROCO{3}]{0}[ R$ {1:.2f}\n\n[TOTAL{3}]{0}[ R$ {2:.2f}'
+              .format(hs, abs(vlp - preco), preco, 12 * ' '))
+
+        r = input('\nDeseja iniciar nova venda? [S/n]: ').lower()
+        if r == 's':
+            realizarVenda()
+        else:
+            menu()
+
+
+##########################################
+# FUNÇÃO QUE BUSCA O PRODUTO PELO CÓDIGO #
+##########################################
+
+def buscarProduto():
+
+    codprod = int(input(f'\n[CÓDIGO DO PRODUTO]{hs}[ '))
+    for x in range(len(produtos)):
+        if codprod == produtos[x][0]:
+            print('[PRODUTO{3}]{0}[ {1}\n[PREÇO{4}]{0}[ R$ {2:.2f}'
+                  .format(hs, produtos[x][1], produtos[x][2], 10 * ' ', 12 * ' '))
+            return True
+
+    print(f"\n{'NENHUM PRODUTO FOI ENCONTRADO':^38}\n")
+    return False
+
+
+#####################
+#   MENU PRINCIPAL  #
+#####################
+
+def menu():
+
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')  # Limpa a tela
+        print(f"{ht}\n{'[ M E N U   P R I N C I P A L ]':^38}\n{ht}\n")
+        resp = int(input('\n'
+                         '\t[1] - Cadastrar produto\n'
+                         '\t[2] - Realizar venda\n\n'
+                         '\t[0] - Sair\n\n{}\n:: '
+                         .format(ht)))
+        if resp == 1:
+            cadastrarProduto()
+        elif resp == 2:
+            realizarVenda()
+        elif resp == 0:
+            exit()
+        else:
+            print(f"{'[ OPÇÃO INVÁLIDA!!! ]':^38}")
+            sleep(1)
+            continue
+
+
+menu()  # Chama o menu
