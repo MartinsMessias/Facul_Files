89eb44988929d73ad7d62b6bb4c0b9051f87c8f9
Messias Martins <35672212+MartinsMessias@users.noreply.github.com>
2019-03-10 21:51:54 -0500

    Create BubbleSort.py
    Algoritmos de Ordenação

---
+++ EstruturasDeDadosI/BubbleSort.py
@@ -0,0 +1,21 @@
+def sort_list(lst):
+    """   Função Bubble Sort  """
+    for j in range(len(lst) - 1):
+        for i in range(len(lst) - 1):
+            if lst[i] > lst[i + 1]:
+                print(lst[i], lst[i + 1])
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+                
+                
+###################################################
+                 EXEMPLO DE USO
+###################################################
+unsorted = [5, 4, 3, 1, 2, 7, 6]
+print('Antes: ', unsorted) # Lista desordenada
+
+# Chama função para ordenar a lista
+sort_list(unsorted)
+
+print('Depois:', unsorted) # Lista ordenada
